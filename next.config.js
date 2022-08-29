31283f5fe53974b72cb321660f9d4dab7cff6979
adevreactjs <adevreactjs@gmail.com>
2022-08-29 15:03:00 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+  images: {
+    domains: ['vignette.wikia.nocookie.net', 'download.logo.wine', 'static-mh.content.disney.io'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,69 @@
-import '../styles/globals.css'
+import React, { useState, useEffect } from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
+import styled from 'styled-components';
+import '../styles/globals.css';
+import Context from '../context';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <AppLoading>
+          <AppLoadingContents>
+            <img
+              src='https://download.logo.wine/logo/Star_Wars/Star_Wars-Logo.wine.png'
+              alt='img'
+            />
+          </AppLoadingContents>
+        </AppLoading>
+      ) : (
+        <>
+          <Context.Provider value={{ person, setPerson, searchResult, setSearchResult }}>
+            <Header />
+            <NavBar />
+            <Component {...pageProps} />
+            <Footer />
+          </Context.Provider>
+        </>
+      )}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > img {
+    object-fit: contain;
+    height: 150px;
+    margin-bottom: 20px;
+    filter: brightness(0) invert(1);
+  }
+`;
